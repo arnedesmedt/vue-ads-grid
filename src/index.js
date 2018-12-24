3293211cd5c0de9fccdc5cfe2e3ea4e75ad36d63
Arne De Smedt <arnedesmdt@gmail.com>
2018-12-24 16:21:52 +0100

    don't export by default for multiple components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './assets/css/tailwind.css';
 import VueAdsGrid from './components/Grid';
 import VueAdsGridItem from './components/GridItem';
 
-export default {
+export {
     VueAdsGrid,
     VueAdsGridItem,
 };
